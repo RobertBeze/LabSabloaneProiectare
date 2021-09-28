9593eadb5a1450ffc6cbeb834a7ea98de31d0ed6
RobertBeze <robert.beze00@e-uvt.ro>
2021-09-28 17:08:58 +0300

    commit

---
+++ MyBook/src/Book.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+    String titlu;
+    List<String> paragraf;
+    List<String> imagini;
+    List<String> tabele;
+
+    public Book(String titlu){
+        this.titlu = titlu;
+        this.paragraf = new ArrayList<>();
+        this.imagini = new ArrayList<>();
+        this.tabele = new ArrayList<>();
+    }
+
+    public void createNewParagraph(String text){
+        this.paragraf.add(text);
+    }
+
+    public void createNewImage(String img){
+        this.imagini.add(img);
+    }
+
+    public void createNewTable(String table){
+        this.tabele.add(table);
+    }
+
+    public void print(){
+        System.out.println("Paragrafele sunt: ");
+        for(String i : this.paragraf)
+            System.out.println(i);
+
+        System.out.println("Imaginile sunt: ");
+        for(String i : this.imagini)
+            System.out.println(i);
+
+        System.out.println("Tabelele sunt: ");
+        for(String i : this.tabele)
+            System.out.println(i);
+    }
+}
